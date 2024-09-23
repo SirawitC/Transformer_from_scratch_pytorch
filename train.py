b4d19444c3f4a583e40af2cfd254ce565250c901
SirawitC <sirawitchok@hotmail.com>
2024-09-23 14:56:51 +0700

    add dataset.py and get dataset fn

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split 
 
+from dataset import BilingualDataset, causal_mask
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -28,14 +30,34 @@ def get_or_guild_tokenizer(config, dataset, lang):
     return tokenizer
 
 def get_dataset(config):
-    dataset_row = load_dataset(f'{config["hf_dataset"]}', f'{config["lang_src"]-{config["lang_tgt"]}}', split="train")
+    dataset_raw = load_dataset(f'{config["hf_dataset"]}', f'{config["lang_src"]-{config["lang_tgt"]}}', split="train")
 
     # build tokenizer
-    tokenizer_src = get_or_guild_tokenizer(config, dataset_row, config["lang_src"])
-    tokenizer_tgt = get_or_guild_tokenizer(config, dataset_row, config["lang_tgt"])
+    tokenizer_src = get_or_guild_tokenizer(config, dataset_raw, config["lang_src"])
+    tokenizer_tgt = get_or_guild_tokenizer(config, dataset_raw, config["lang_tgt"])
 
     # train test split 
-    train_split_len = int(float(config["train_split_ratio"]) * len(dataset_row))
-    test_split_len = len(dataset_row) - train_split_len
+    train_split_len = int(float(config["train_split_ratio"]) * len(dataset_raw))
+    test_split_len = len(dataset_raw) - train_split_len
 
-    train_split, test_split = random_split(dataset_row, [train_split_len, test_split_len])
+    train_split, test_split = random_split(dataset_raw, [train_split_len, test_split_len])
+
+    train_dataset = BilingualDataset(train_split, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    test_dataset = BilingualDataset(test_split, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in dataset_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+
+    return train_dataloader, test_dataloader, tokenizer_src, tokenizer_tgt

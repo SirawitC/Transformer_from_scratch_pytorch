dfdd81e0e718b1b0e980b6ac57797f0fdbd50863
SirawitC <sirawitchok@hotmail.com>
2024-09-24 10:30:04 +0700

    add validation loop and finish training loop

--- train.py
+++ train.py
@@ -73,6 +73,74 @@ def get_model(config, vocab_src_len, vocab_tgt_len):
     model = build_transformer(vocab_src_len, vocab_tgt_len, config["seq_len"], config["seq_len"], config["d_model"])
     return model
 
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it for every token we get from the decoder
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with the sos token
+    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+        
+        # Build mask for the target (decoder input)
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+
+        # Calculate the output of the decoder
+        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+
+        # Get the next token
+        prob = model.project(out[:, -1])
+        # Select the token with max probability
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat([decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
+
+        if next_word == eos_idx:
+            break
+    
+    return decoder_input.squeeze(0)
+
+def run_validation(model, validation_dataset, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_state, writer, num_examples=2):
+    model.eval()
+    
+    count = 0
+    
+    source_texts = []
+    expected_texts = []
+    predicted_texts = []
+
+    console_width = 80
+
+    with torch.no_grad():
+        for batch in validation_dataset:
+            count += 1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+
+            assert encoder_input.size(0) == 1, "Batch size must be 1 for validation"
+
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            expected_texts.append(target_text)
+            predicted_texts.append(model_out_text)
+
+            # Print message to the console
+            print_msg('-'*console_width)
+            print_msg(f'Source: {source_text}')
+            print_msg(f'Target: {target_text}')
+            print_msg(f'Predicted: {model_out_text}')
+
+            if count == num_examples:
+                break
+
+
 def train_model(config):
     # Define the device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -101,10 +169,11 @@ def train_model(config):
     loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
 
     for epoch in range(initial_epoch, config["num_epochs"]):
-        model.train()
         batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoch: 02d}")
 
         for batch in batch_iterator:
+            model.train()
+
             encoder_input = batch["encoder_input"].to(device) # (batch, seq_len)
             decoder_input = batch["decoder_input"].to(device) # (batch, seq_len)
             encoder_mask = batch["encoder_mask"].to(device) # (batch, 1, 1, seq_len)
@@ -134,6 +203,8 @@ def train_model(config):
 
             global_step += 1
 
+        run_validation(model, test_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+        
         # Save the model
         model_filename = get_weights_file_path(config, f'{epoch:02d}')
         torch.save({
